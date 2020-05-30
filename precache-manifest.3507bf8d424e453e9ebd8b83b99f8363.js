3948d80986225015742bf0954459c16c9cb6a808
Brian Wu <bwu@Brians-MacBook-Pro-2.local>
2020-05-30 14:25:46 -0500

    Updates

---
+++ precache-manifest.3507bf8d424e453e9ebd8b83b99f8363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadba6af7b206dafa53ec3bf3a082c0c",
+    "url": "/react_covid19_deploy/index.html"
+  },
+  {
+    "revision": "ed95aab6f6d8af9d53dd",
+    "url": "/react_covid19_deploy/static/css/main.a78cdfe6.chunk.css"
+  },
+  {
+    "revision": "f6968be011f7bde1c168",
+    "url": "/react_covid19_deploy/static/js/2.6cf9ddf0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react_covid19_deploy/static/js/2.6cf9ddf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed95aab6f6d8af9d53dd",
+    "url": "/react_covid19_deploy/static/js/main.d02658e1.chunk.js"
+  },
+  {
+    "revision": "828eba2dd7da336443e5",
+    "url": "/react_covid19_deploy/static/js/runtime-main.817a71bc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react_covid19_deploy/static/media/image.d7265326.png"
+  }
+]);
